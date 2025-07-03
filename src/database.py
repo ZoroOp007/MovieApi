6393fb3de9486e529105879681bea3181a0e3065
ZoroOp007 <bkp825301@gmail.com>
2025-07-03 08:05:11 +0530

    created dot env variable

--- src/database.py
+++ src/database.py
@@ -2,8 +2,13 @@ from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
 
+from dotenv import load_dotenv
+
+load_dotenv()
+import os
+
 #this line is to connect our app with the postgres database
-SQLALCHEMY_DATABASE_URL = 'postgresql://user:qUzj02TINfCknYHJvVRbOKagWNEiK4jL@dpg-d1itceadbo4c73bmsb0g-a.oregon-postgres.render.com/fastapi_ge19'
+SQLALCHEMY_DATABASE_URL = os.getenv('postgres_url')
 
 engine = create_engine(SQLALCHEMY_DATABASE_URL, pool_pre_ping=True)
 SessionLocal = sessionmaker(bind=engine, autoflush=False)
