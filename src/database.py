6c853aafbfa16e29744818a694fe03d3d88759da
ZoroOp007 <bkp825301@gmail.com>
2025-07-06 01:29:41 +0530

    db_url

--- src/database.py
+++ src/database.py
@@ -7,7 +7,7 @@ from dotenv import load_dotenv
 load_dotenv()
 
 #this line is to connect our app with the postgres database
-SQLALCHEMY_DATABASE_URL = os.getenv("postgres_url")
+SQLALCHEMY_DATABASE_URL = "postgresql://user:qUzj02TINfCknYHJvVRbOKagWNEiK4jL@dpg-d1itceadbo4c73bmsb0g-a.oregon-postgres.render.com/fastapi_ge19"
 
 engine = create_engine(SQLALCHEMY_DATABASE_URL, pool_pre_ping=True)
 SessionLocal = sessionmaker(bind=engine, autoflush=False)
