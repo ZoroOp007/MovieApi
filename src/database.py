130bd1929765fe56e175395ac1236896b04fcf26
ZoroOp007 <bkp825301@gmail.com>
2025-07-06 01:15:50 +0530

    updated requirements.txt

--- src/database.py
+++ src/database.py
@@ -2,13 +2,12 @@ from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
 
-from dotenv import load_dotenv
-
-load_dotenv()
 import os
+from dotenv import load_dotenv
+load_dotenv()
 
 #this line is to connect our app with the postgres database
-SQLALCHEMY_DATABASE_URL = os.getenv('postgres_url')
+SQLALCHEMY_DATABASE_URL = os.getenv("postgres_url")
 
 engine = create_engine(SQLALCHEMY_DATABASE_URL, pool_pre_ping=True)
 SessionLocal = sessionmaker(bind=engine, autoflush=False)
